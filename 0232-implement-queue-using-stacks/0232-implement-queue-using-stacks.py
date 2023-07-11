da58a9766ea1de35cb95706f60e6a28cd24ad16a
Youngdo Lee <lyd0531@kaist.ac.kr>
2023-07-11 17:31:27 +0900

    Time: 18 ms (46.31%), Space: 13.3 MB (94.9%) - LeetHub

---
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.py
@@ -0,0 +1,66 @@
+class MyQueue(object):
+
+    def __init__(self):
+        self.top = None; self.size = 0
+        self.queue = []
+        self.ready = []
+
+    def push(self, x):
+        """
+        :type x: int
+        :rtype: None
+        """
+        self.ready.append(x)
+
+        # Queue size update
+        self.size += 1
+
+        # Top value update
+        self.top = self.queue[-1] if self.queue else self.ready[0]
+        
+
+    def pop(self):
+        """
+        :rtype: int
+        """
+        if self.size == 0:
+            return -1
+        
+        if len(self.queue) == 0:
+            while self.ready:
+                self.queue.append(self.ready.pop())
+
+        item = self.queue.pop()
+
+        # Queue size update
+        self.size -= 1
+
+        # Top value update
+        if self.size == 0:
+            self.top = None
+        elif self.queue:
+            self.top = self.queue[-1]
+        else:
+            self.top = self.ready[0]
+
+        return item
+
+    def peek(self):
+        """
+        :rtype: int
+        """
+        return self.top
+
+    def empty(self):
+        """
+        :rtype: bool
+        """
+        return self.size == 0    
+
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
