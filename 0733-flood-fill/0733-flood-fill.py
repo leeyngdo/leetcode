26527e6be28e9f3a6291e2ac5d29231ad217c295
Youngdo Lee <lyd0531@kaist.ac.kr>
2023-07-13 22:14:59 +0900

    Time: 60 ms (52.69%), Space: 13.1 MB (99.71%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.py
+++ 0733-flood-fill/0733-flood-fill.py
@@ -9,24 +9,25 @@ class Solution(object):
         :type color: int
         :rtype: List[List[int]]
         """
-        m = len(image); n = len(image[0]); target = image[sr][sc]
+        target = image[sr][sc]
         if target == color: return image
 
-        queue = deque(); queue.append((sr, sc)); image[sr][sc] = color
-        while queue:
-            h, w = queue.popleft()
+        m = len(image); n = len(image[0])
+        stack = deque(); stack.append((sr, sc)); image[sr][sc] = color
+        while stack:
+            h, w = stack.pop()
             if h > 0 and image[h-1][w] == target:
                 image[h-1][w] = color
-                queue.append((h-1, w))
+                stack.append((h-1, w))
             if h < m - 1 and image[h+1][w] == target:
                 image[h+1][w] = color
-                queue.append((h+1, w))
+                stack.append((h+1, w))
             if w > 0 and image[h][w-1] == target:
                 image[h][w-1] = color
-                queue.append((h, w-1))
+                stack.append((h, w-1))
             if w < n - 1 and image[h][w+1] == target:
                 image[h][w+1] = color
-                queue.append((h, w+1))
+                stack.append((h, w+1))
 
         return image
             
